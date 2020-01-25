736f5c423058739dbb459f8ae4b8c04ae7240b63
Cesar Montero <monterosr24@gmail.com>
2020-01-25 07:07:06 -0600

    add project of repositoty

---
+++ PolicyManagement.Repository/Interfaces/IRepository.cs
@@ -0,0 +1,30 @@
+﻿using PolicyManagement.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace PolicyManagement.Repository.Interfaces
+{
+    public interface IRepository
+    {
+        TEntity Create<TEntity>(TEntity entity, string createdBy)
+           where TEntity : BaseModel;
+
+        void Update<TEntity>(TEntity entity, string modifiedBy)
+            where TEntity : BaseModel;
+
+        void Delete<TEntity>(object id)
+            where TEntity : BaseModel;
+
+        void Delete<TEntity>(TEntity entity)
+            where TEntity : BaseModel;
+
+        IEnumerable<TEntity> GetAll<TEntity>(Expression<Func<TEntity, bool>> predicate)
+           where TEntity : BaseModel;
+
+        void Save();
+
+        Task SaveAsync();
+    }
+}
---
+++ PolicyManagement.Repository/Repository.cs
@@ -0,0 +1,84 @@
+﻿using PolicyManagement.Models.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+
+namespace PolicyManagement.Repository.Interfaces
+{
+    public class Repository<TContext> : IRepository where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public Repository(TContext context)
+        {
+            _context = context;
+        }
+
+        public TEntity Create<TEntity>(TEntity entity, string createdBy) where TEntity : BaseModel
+        {
+            entity.CreatedDate = DateTime.Now;
+            entity.CreatedUser = createdBy;
+            return _context.Set<TEntity>().Add(entity);
+        }
+
+        public void Update<TEntity>(TEntity entity, string modifiedBy) where TEntity : BaseModel
+        {
+            entity.ModifydDate = DateTime.Now;
+            entity.UpdatedUser = modifiedBy;
+            _context.Set<TEntity>().Attach(entity);
+            _context.Entry(entity).State = EntityState.Modified;
+        }
+
+        public void Delete<TEntity>(object id) where TEntity : BaseModel
+        {
+            TEntity entity = _context.Set<TEntity>().Find(id);
+            Delete(entity);
+        }
+
+        public void Delete<TEntity>(TEntity entity) where TEntity : BaseModel
+        {
+            var dbSet = _context.Set<TEntity>();
+
+            if(_context.Entry(entity).State == EntityState.Detached)
+            {
+                dbSet.Attach(entity);
+            }
+
+            dbSet.Remove(entity);
+        }
+
+        public IEnumerable<TEntity> GetAll<TEntity>(Expression<Func<TEntity, bool>> predicate) where TEntity : BaseModel
+        {
+            var dbSet = _context.Set<TEntity>();
+            return dbSet.Where(predicate).ToList();
+        }
+
+        public void Save()
+        {
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        public Task SaveAsync()
+        {
+            try
+            {
+                return _context.SaveChangesAsync();
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
