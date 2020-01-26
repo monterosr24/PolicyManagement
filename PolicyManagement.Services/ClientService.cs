bdd5f7bf401637d5739d81e1abb91ff816b6970a
Cesar Montero <monterosr24@gmail.com>
2020-01-26 13:19:39 -0600

    add service of client

---
+++ PolicyManagement.Services/ClientService.cs
@@ -0,0 +1,16 @@
+﻿using PolicyManagement.Models.Model;
+using PolicyManagement.Models.ModelView;
+using PolicyManagement.Services.Interface;
+using System.Data.Entity;
+
+namespace PolicyManagement.Services
+{
+    public class ClientService: BaseService<Client,ClientViewModel>, IClientService
+    {
+        public ClientService(DbContext context)
+            :base(context)
+        {
+
+        }
+    }
+}
---
+++ PolicyManagement.Services/Interface/IClientService.cs
@@ -0,0 +1,8 @@
+﻿using PolicyManagement.Models.ModelView;
+
+namespace PolicyManagement.Services.Interface
+{
+    public interface IClientService: IBaseService<ClientViewModel>
+    {
+    }
+}
