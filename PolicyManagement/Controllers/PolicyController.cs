d447b2cb24dc2233a537cee257ef1f353994adb0
Cesar Montero <monterosr24@gmail.com>
2020-01-26 12:43:10 -0600

    remove garbage code in the policy controller

--- PolicyManagement/Controllers/PolicyController.cs
+++ PolicyManagement/Controllers/PolicyController.cs
@@ -35,65 +35,11 @@ namespace PolicyManagement.Controllers
         {
             return View();
         }
-
-        // POST: Policy/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
+        
         // GET: Policy/Edit/5
         public ActionResult Edit(int id)
         {
             return View();
-        }
-
-        // POST: Policy/Edit/5
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        // GET: Policy/Delete/5
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        // POST: Policy/Delete/5
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
         }
     }
 }
