01aeacd6197e000f12ce7d93e48d85e2f2c1cc26
Cesar Montero <monterosr24@gmail.com>
2020-01-26 13:50:34 -0600

    crud of client

--- PolicyManagement.Models/ModelView/ClientViewModel.cs
+++ PolicyManagement.Models/ModelView/ClientViewModel.cs
@@ -1,17 +1,18 @@
-﻿
-using PolicyManagement.Models.ModelView;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace PolicyManagement.Models.ModelView
 {
     public class ClientViewModel: BaseViewModel
     {
+        [Required]
         [Display(Name = "Nombre")]
         public string Name { get; set; }
 
+        [Required]
         [Display(Name = "Apellidos")]
         public string LastName { get; set; }
 
+        [Required]
         [Display(Name = "Dirección")]
         public string Direction { get; set; }
     }
