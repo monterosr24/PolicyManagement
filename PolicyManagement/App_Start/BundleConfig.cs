5ff965eea796dce15b7d54d8510f8619c325c3a2
Cesar Montero <monterosr24@gmail.com>
2020-01-26 14:44:55 -0600

    add datepicker

--- PolicyManagement/App_Start/BundleConfig.cs
+++ PolicyManagement/App_Start/BundleConfig.cs
@@ -20,11 +20,13 @@ namespace PolicyManagement
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/jquery-ui.min.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/jquery-ui.min.css"));
         }
     }
 }
