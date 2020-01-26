9b6379d9fbcba5023e26830fd867a36f547eb54d
Cesar Montero <monterosr24@gmail.com>
2020-01-25 18:38:36 -0600

    add test project PolicyManagement.Services

---
+++ PolicyManagement.Tests/PolicyManagement.Services/BaseTestService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using Effort;
+using PolicyManagement.Data;
+using PolicyManagement.Models.Mapper;
+using PolicyManagement.Models.Model;
+using System;
+using System.Collections.Generic;
+
+namespace PolicyManagement.Tests.PolicyManagement.Services
+{
+    public class BaseTestService
+    {
+        protected ApplicationDbContext Context { get; }
+
+        public BaseTestService()
+        {
+            var connection = DbConnectionFactory.CreateTransient();
+            Context = new ApplicationDbContext(connection);
+            InitializeAutoMapper();
+        }
+
+        private static void InitializeAutoMapper()
+        {
+            Mapper.Initialize(cfg => {
+                    cfg.AddProfile<MappingProfile>();
+            });
+        }
+
+        protected void PopulateMemoryDb()
+        {
+            var typeRisks = new List<TypeRisk>
+            {
+                new TypeRisk() { Type = "Bajo", Description = "Poliza de riesgo baja", CreatedDate = DateTime.Now, CreatedUser = "Admin" },
+                new TypeRisk() { Type = "Medio,", Description = "Poliza de riesgo medio,", CreatedDate = DateTime.Now, CreatedUser = "Admin" },
+                new TypeRisk() { Type = "Medio Alto", Description = "Poliza de riesgo media-alta", CreatedDate = DateTime.Now, CreatedUser = "Admin" },
+                new TypeRisk() { Type = "Alto", Description = "Poliza de riesgo alto", CreatedDate = DateTime.Now, CreatedUser = "Admin" }
+            };
+
+            var typeCovering = new List<TypeCovering>
+            {
+                new TypeCovering() { Name = "Terremoto", Percentage = 80, CreatedDate = DateTime.Now, CreatedUser = "Admin" },
+                new TypeCovering() { Name = "Incendio", Percentage = 70, CreatedDate = DateTime.Now, CreatedUser = "Admin" },
+                new TypeCovering() { Name = "Robo,", Percentage = 50, CreatedDate = DateTime.Now, CreatedUser = "Admin" },
+                new TypeCovering() { Name = "Pérdida,", Percentage = 20, CreatedDate = DateTime.Now, CreatedUser = "Admin" }
+            };
+            
+            
+            Context.TypeRisk.AddRange(typeRisks);
+            Context.TypeCovering.AddRange(typeCovering);
+            Context.SaveChanges();
+        }
+    }
+}
---
+++ PolicyManagement.Tests/PolicyManagement.Services/PolicyServiceTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PolicyManagement.Models.ModelView;
+using PolicyManagement.Services;
+using System;
+
+namespace PolicyManagement.Tests.PolicyManagement.Services
+{
+    [TestClass]
+    public class PolicyServiceTest :BaseTestService
+    {
+        [TestInitialize]
+        public void Init()
+        {
+            PopulateMemoryDb();
+        }
+
+        [TestMethod]
+        public void BaseServiceCreateTest()
+        {
+            var policyService = new PolicyService(Context);
+
+            var policy = new PolicyViewModel { Name = "Poliza 1", Description = "Son pruebas", IdTypeCovering = 1, IdTypeRisk = 1, StartPolicy = DateTime.Now, Period = 2, Price = 1000 };
+            policy = policyService.Create(policy, "Admin");
+
+            Assert.AreEqual(policy.Id, 1);
+            Assert.AreEqual(policy.Name, "Poliza 1");
+        }
+
+    }
+}
